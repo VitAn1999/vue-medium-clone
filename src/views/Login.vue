61e8c5a8bab4d7850d8daf9658d448682778a6aa
Рощупкин Виталий <elpistolero1999@gmail.com>
2021-03-20 17:10:57 +0300

    create Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,46 @@
 <template>
-  <div>Sign in</div>
+  <div class="auth-page">
+    <div class="container-page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign In</h1>
+          <p class="text-xs-center">
+            <router-link :to="{ name: 'register' }"
+              >Need an account?</router-link
+            >
+          </p>
+          <form>
+            <fieldset class="form-group">
+              <input
+                type="email"
+                class="form-control form-control-lg"
+                placeholder="Email"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                type="password"
+                class="form-control form-control-lg"
+                placeholder="Password"
+              />
+            </fieldset>
+            <button type="submit" class="btn btn-lg btn-primary pull-xs-right">
+              Sign in
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'AppLogin',
+  methods: {
+    onSubmit() {
+      console.log('submitted form');
+    }
+  }
+};
+</script>
